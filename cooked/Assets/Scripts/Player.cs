ba74f486b675a33fe4c21e14f3b3a557b297fc4e
Kinga Zawarty <niu.ula.a@gmail.com>
2024-11-25 19:51:57 +0100

    movement legacy input, ciekawostki

---
+++ cooked/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0f, 0f);
+
+        if( Input.GetKey(KeyCode.W))
+        {
+            inputVector.y += 1f;
+        }
+
+        if( Input.GetKey(KeyCode.S))
+        {
+            inputVector.y -= 1f;
+        }
+
+        if( Input.GetKey(KeyCode.D))
+        {
+            inputVector.x += 1f;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            inputVector.x -= 1f;
+        }
+
+        inputVector = inputVector.normalized;
+
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        // ciekawostki: 
+        // bez normalized ruch po przekatnej bylby szybszy - vector 1,1 jest dluzszy, wiec musimy znormalizowac by zawsze byla ta sama dlugosc
+        // bez Time.deltaTime player poruszalby sie w roznnej predkosci w roznych FPS
+    }
+}
