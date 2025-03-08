654d9d67c29f7db0f7066efa572742462e4d62ee
Kinga Zawarty <niu.ula.a@gmail.com>
2025-03-08 13:11:51 +0100

    plate visual spawning

--- cooked/Assets/Scripts/Counters/PlatesCounter.cs
+++ cooked/Assets/Scripts/Counters/PlatesCounter.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlatesCounter : BaseCounter
 {
+    public event EventHandler OnPlateSpawned;
+
     [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
-    private float spawnPlateTimer;
+
+    private float spawnPlateTimer = 3.9f;
     private float spawnPlateTimerMax = 4f;
+    private int platesSpawnedAmount = 0;
+    private int platesSpawnedAmountMax = 4;
+
 
     private void Update()
     {
         spawnPlateTimer += Time.deltaTime;
         if(spawnPlateTimer > spawnPlateTimerMax)
         {
-            KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, this);
+            spawnPlateTimer = 0f;
+
+            if(platesSpawnedAmount < platesSpawnedAmountMax)
+            {
+                platesSpawnedAmount ++;
+                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+            
+            //KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, this);
         }
     }
     
---
+++ cooked/Assets/Scripts/Counters/PlatesCounterVisual.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatesCounterVisual : MonoBehaviour
+{
+    [SerializeField] private PlatesCounter platesCounter;
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private Transform plateVisualPrefab;
+
+    private List<GameObject> plateVisuals = new List<GameObject>();
+
+    private void Start()
+    {
+        platesCounter.OnPlateSpawned += PlatesCounter_OnPlateSpawned;
+    }
+
+    private void PlatesCounter_OnPlateSpawned(object sender, System.EventArgs e)
+    {
+        var plate = Instantiate(plateVisualPrefab, counterTopPoint);
+        var plateOffset = 0.1f * plateVisuals.Count;
+        plate.localPosition = new Vector3(0, plateOffset, 0);
+        plateVisuals.Add(plate.gameObject);
+    }
+
+}
