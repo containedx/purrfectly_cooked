fe0b729fd380033ae2ee1b81bd15a532dd99364f
Kinga Zawarty <niu.ula.a@gmail.com>
2025-03-09 19:22:53 +0100

    plate visual

--- cooked/Assets/Scripts/KitchenObjects/Plate.cs
+++ cooked/Assets/Scripts/KitchenObjects/Plate.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Plate : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs 
+    {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+
+
     [SerializeField] private List<KitchenObjectSO> recipeIngredients; 
     private List<KitchenObjectSO> kitchenObjectSOList = new List<KitchenObjectSO>();
 
@@ -14,6 +22,9 @@ public class Plate : KitchenObject
         if(kitchenObjectSOList.Contains(kitchenObjectSO)) return false;
 
         kitchenObjectSOList.Add(kitchenObjectSO);
+        OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs { kitchenObjectSO = kitchenObjectSO});
+
+
         return true;
     }
 }
---
+++ cooked/Assets/Scripts/KitchenObjects/PlateCompleteVisual.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlateCompleteVisual : MonoBehaviour
+{
+    [Serializable]
+    public struct KitchenObjectSO_GameObject 
+    {
+        public KitchenObjectSO kitchenObjectSO;
+        public GameObject gameObject;
+    }
+
+    [SerializeField] private Plate plate;
+    [SerializeField] private List<KitchenObjectSO_GameObject> kitchenObjectSOGameObjectList = new List<KitchenObjectSO_GameObject>();
+
+    private void Start()
+    {
+        plate.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, Plate.OnIngredientAddedEventArgs e)
+    {
+        foreach(var obj in kitchenObjectSOGameObjectList)
+        {
+            if(obj.kitchenObjectSO == e.kitchenObjectSO)
+            {
+                obj.gameObject.SetActive(true);
+            }
+        }
+    }
+}
